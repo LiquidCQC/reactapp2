e284f89e284936607ac9648a8712c6d815658f47
LiquidCQC <84178150+LiquidCQC@users.noreply.github.com>
2024-06-14 19:45:56 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,26 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import { AwsRum } from 'aws-rum-web';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World =)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 let awsRum = null;
 try {
   const config = {
@@ -37,7 +17,7 @@ try {
   const APPLICATION_VERSION = '1.0.0';
   const APPLICATION_REGION = 'eu-west-3';
 
-  const awsRum = new AwsRum(
+  awsRum = new AwsRum(
     APPLICATION_ID,
     APPLICATION_VERSION,
     APPLICATION_REGION,
@@ -46,5 +26,47 @@ try {
 } catch (error) {
   // Ignore errors thrown during CloudWatch RUM web client initialization
 }
-export default App;
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <p><Link to="/">Home</Link></p>
+        <p><Link to="/about">About</Link></p>
+        <p><Link to="/users">Users</Link></p>
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/user/*" element={<User />} />
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <div>
+    <h2>Users</h2>
+    <p><Link to="/user/1">User 1</Link></p>
+    <p><Link to="/user/2">User 2</Link></p>
+    <p><Link to="/user/3">User 3</Link></p>
+  </div>
+  ;
+}
+
+function User() {
+  const location = useLocation();
+  const user = location.pathname.split('/').pop();
+  return <h2>User: {user}</h2>;
+}
+
 
