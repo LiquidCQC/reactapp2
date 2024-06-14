3f1333e28a6ccc41b17ce3ff51d1638d15e55387
LiquidCQC <84178150+LiquidCQC@users.noreply.github.com>
2024-06-14 14:53:54 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  useLocation
-} from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
 import { AwsRum } from 'aws-rum-web';
 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World =)
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 let awsRum = null;
 try {
-  const config: AwsRumConfig = {
+  const config = {
     sessionSampleRate: 1,
     identityPoolId: "eu-west-3:af8d455a-0128-414a-b04f-3d2cce2e8ff0",
     endpoint: "https://dataplane.rum.eu-west-3.amazonaws.com",
@@ -19,11 +33,11 @@ try {
     enableXRay: false
   };
 
-  const APPLICATION_ID: string = 'feabfc04-03d6-4c1c-b1ec-feccbad38a9f';
-  const APPLICATION_VERSION: string = '1.0.0';
-  const APPLICATION_REGION: string = 'eu-west-3';
+  const APPLICATION_ID = 'feabfc04-03d6-4c1c-b1ec-feccbad38a9f';
+  const APPLICATION_VERSION = '1.0.0';
+  const APPLICATION_REGION = 'eu-west-3';
 
-  const awsRum: AwsRum = new AwsRum(
+  const awsRum = new AwsRum(
     APPLICATION_ID,
     APPLICATION_VERSION,
     APPLICATION_REGION,
@@ -32,51 +46,5 @@ try {
 } catch (error) {
   // Ignore errors thrown during CloudWatch RUM web client initialization
 }
-export default function App() {
-  return (
-    <Router>
-      <div>
-        <p><Link to="/">Home</Link></p>
-        <p><Link to="/about">About</Link></p>
-        <p><Link to="/users">Users</Link></p>
-        <p><Link to="/welcome">Welcome</Link></p>
-        <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/user/*" element={<User />} />
-          <Route path="/welcome" element={<Welcome />} />
-          <Route exact path="/" element={<Home />} />
-        </Routes>
-      </div>
-    </Router>
-  )
-}
+export default App;
 
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <div>
-    <h2>Users</h2>
-    <p><Link to="/user/1">User 1</Link></p>
-    <p><Link to="/user/2">User 2</Link></p>
-    <p><Link to="/user/3">User 3</Link></p>
-  </div>
-  ;
-}
-
-function User() {
-  const location = useLocation();
-  const user = location.pathname.split('/').pop();
-  return <h2>User: {user}</h2>;
-}
-
-function Welcome() {
-  // deliberate error
-  return <h2>Welcome {this.subject.toUpperCase()}</h2>;
-}
