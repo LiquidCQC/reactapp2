2d8253244e2719a631455aba7798e1eea2abd721
LiquidCQC <84178150+LiquidCQC@users.noreply.github.com>
2024-06-15 07:52:14 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'; // Import React for JSX syntax
 import logo from './logo.svg';
 import './App.css';
 import { AwsRum } from 'aws-rum-web';
@@ -9,7 +10,7 @@ try {
     sessionSampleRate: 1,
     identityPoolId: "eu-west-3:af8d455a-0128-414a-b04f-3d2cce2e8ff0",
     endpoint: "https://dataplane.rum.eu-west-3.amazonaws.com",
-    telemetries: ["performance","errors","http"],
+    telemetries: ["performance", "errors", "http"],
     allowCookies: true,
     enableXRay: false
   };
@@ -28,13 +29,19 @@ try {
   // Ignore errors thrown during CloudWatch RUM web client initialization
 }
 
-export default function App() {
+const App = () => {
   return (
     <Router>
-      <div>
-        <p><Link to="/">Home</Link></p>
-        <p><Link to="/about">About</Link></p>
-        <p><Link to="/users">Users</Link></p>
+      <div className="App"> {/* Apply CSS class for styling */}
+        <header className="App-header"> {/* Define a header section */}
+          <img src={logo} className="App-logo" alt="React Logo" /> {/* Render React logo */}
+          <h1>Hello!</h1> {/* Add the "Hello World" message */}
+        </header>
+        <nav>
+          <p><Link to="/">Home</Link></p>
+          <p><Link to="/about">About</Link></p>
+          <p><Link to="/users">Users</Link></p>
+        </nav>
         <Routes>
           <Route path="/about" element={<About />} />
           <Route path="/users" element={<Users />} />
@@ -43,7 +50,7 @@ export default function App() {
         </Routes>
       </div>
     </Router>
-  )
+  );
 }
 
 function Home() {
@@ -55,13 +62,14 @@ function About() {
 }
 
 function Users() {
-  return <div>
-    <h2>Users</h2>
-    <p><Link to="/user/1">User 1</Link></p>
-    <p><Link to="/user/2">User 2</Link></p>
-    <p><Link to="/user/3">User 3</Link></p>
-  </div>
-  ;
+  return (
+    <div>
+      <h2>Users</h2>
+      <p><Link to="/user/1">User 1</Link></p>
+      <p><Link to="/user/2">User 2</Link></p>
+      <p><Link to="/user/3">User 3</Link></p>
+    </div>
+  );
 }
 
 function User() {
@@ -70,4 +78,4 @@ function User() {
   return <h2>User: {user}</h2>;
 }
 
-
+export default App;
